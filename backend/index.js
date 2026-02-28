a6e13c5d4612be489f127864c399bbd9ae899a19
adam-shen <113874468+adam-shen@users.noreply.github.com>
2026-02-28 13:59:30 -0500

    corrected string syntax in server log and added start script to run backend with "npm start"

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 //start server
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log('Server listening on port ${port}');
+    console.log(`Server listening on port ${port}`);
 })
 
 
