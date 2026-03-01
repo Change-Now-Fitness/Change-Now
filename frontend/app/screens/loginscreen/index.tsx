635d047a74c4b022cbd0ccc0cb6e10ebbec977e8
Samuel <sam9449jm@gmail.com>
2026-03-01 16:12:16 -0500

    added basic ui for login so i can work on and test backend

--- frontend/app/screens/loginscreen/index.tsx
+++ frontend/app/screens/loginscreen/index.tsx
@@ -1,17 +1,51 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, TextInput, View, StyleSheet, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function LoginScreen() {
+
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const handleLogin = () => {
-        console.log("Login Clicked")
+        console.log("Log in Clicked")
     }
+    const handleSignup= () => {
+        console.log("Sign up Clicked")
+    }
+ 
+    
     return (
         <View style = {styles.container}>
             <Text style = {styles.title}>Change Now</Text>
+            <TextInput
+                style = {styles.input}
+                placeholder = "Email"
+                placeholderTextColor="#666"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                keyboardType="email-address"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder = "Password"
+                placeholderTextColor = "#666"
+                value = {password}
+                onChangeText = {setPassword}
+                secureTextEntry
+                />
             <Pressable 
                 style = {styles.loginButton}
                 onPress = {handleLogin}
             >
-               <Text style = {styles.loginButtonText}>Log in</Text>
+               <Text style = {styles.loginButtonText}>Log In</Text>
+            </Pressable>
+             <Pressable 
+                style = {styles.loginButton}
+                onPress = {handleSignup}
+            >
+               <Text style = {styles.loginButtonText}>Sign Up</Text>
             </Pressable>
         
         </View>
@@ -19,6 +53,13 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
+    input: {
+        backgroundColor: "#f5f5f5",
+        color: "#000000",
+        margin: 10,
+        padding: 5,
+        marginBottom: 30 
+    },
     container: {
         flex: 1,
         justifyContent: "flex-start",
@@ -29,7 +70,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 54,
         fontWeight: "bold",
-        margin: 20,
+        margin: 40,
         color: "#000000",
   
     },
@@ -44,6 +85,7 @@ const styles = StyleSheet.create({
         padding: 20,
         width: 200,
         alignItems: "center",
-        borderRadius: 50
+        borderRadius: 50,
+        margin: 10
     }
 });
