be176f457fc5e178e3c7a0357368ed3d1bb629ce
Samuel <sam9449jm@gmail.com>
2026-03-02 15:26:09 -0500

    edited auth route with anti sql injection stuff

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-//connect to postgress with pg
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = require('./dbconnection');
+
 
 //test route
 app.get('/', (req, res) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,24 @@
 
 //password hasher
 const argon2 = require("argon2");
+const pool = require('../dbconnection')
 //token library
-const jwt = require('jsonwebtoken');
 
+const jwt = require('jsonwebtoken');
+//secret code tbd, 'dev-secret' by defualt
+const JWT_SECRET = process.env.JWT_SECRET || 'dev-secret'; 
+
+//auth
+app.post('/auth/signup', async (req, res) => {
+    //ensure required fields filled, else returns json w error
+    if (email == "" || password == "") {
+        return res.status(400).json({ error: 'Email and Password required'});
+    }
+
+    try {
+        const tryExistingEmail = await pool.query(
+            'SELECT id FROM users WHERE email = $1',
+            [email]
+        );
+    }
+})
