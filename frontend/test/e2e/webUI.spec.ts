c42f0139026c78d247d405fd5cec36c1daebf2cd
Samuel <sam9449jm@gmail.com>
2026-04-19 13:56:47 -0400

    added timeouts to web e2e

--- frontend/test/e2e/webUI.spec.ts
+++ frontend/test/e2e/webUI.spec.ts
@@ -6,10 +6,10 @@ test("User can log in and log out", async ({ page }) => {
   await page.getByTestId("password").fill("password");
   await page.getByRole("button", { name: /log in/i }).click();
   await page.goto("/userscreen");
-  await expect(page.getByText(/Welcome/i)).toBeVisible();
-  await expect(page.getByText(/logged in/i)).toBeVisible();
+  await expect(page.getByText(/Welcome/i)).toBeVisible({timeout: 15000});
+  await expect(page.getByText(/logged in/i)).toBeVisible({timeout: 15000});
   await page.getByRole("button", { name: /log out/i }).click();
-  await expect(page.getByText(/log in/i)).toBeVisible();
+  await expect(page.getByText(/log in/i)).toBeVisible({timeout: 15000});
   await page.goto("/userscreen");
-  await expect(page.getByText(/log in/i)).toBeVisible();
+  await expect(page.getByText(/log in/i)).toBeVisible({timeout: 15000});
 });
